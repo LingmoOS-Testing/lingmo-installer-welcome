765739d20b85a0be18adbfd21bf5b6d9e1fdbf01
chun-awa <110163919+chun-awa@users.noreply.github.com>
2024-09-07 11:45:48 +0800

    Initial Commit

---
+++ lingmo-installer-welcome.py
@@ -0,0 +1,119 @@
+import gi
+import subprocess
+
+gi.require_version("Gtk", "3.0")
+from gi.repository import Gtk, Gdk
+
+
+class WelcomeWindow(Gtk.Window):
+    def __init__(self):
+        super().__init__(title="Welcome")
+        self.set_border_width(60)
+        self.set_decorated(False)
+        self.set_resizable(False)
+        self.set_default_size(500, 450)
+
+        self.apply_css()
+
+        vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=10)
+        self.add(vbox)
+
+        header_label = Gtk.Label(label="Lingmo OS Installer")
+        header_label.get_style_context().add_class("header")
+        vbox.pack_start(header_label, False, False, 0)
+
+        buttons = [
+            [
+                "lingmo-core",
+                "Install Lingmo OS",
+                "Install Lingmo OS on your disk",
+                ["calamares"],
+            ],
+            [
+                "web-browser",
+                "Open Browser",
+                "Use the system's default browser",
+                ["firefox"],
+            ],
+            [
+                "disk-baob",
+                "Disk Partitioning",
+                "Use the disk partitioning tool to partition the disk",
+                ["gparted"],
+            ],
+        ]
+        for i in buttons:
+            vbox.pack_start(self.create_button(*i), True, True, 0)
+
+        self.connect("delete-event", lambda widget, event: True)
+
+    def apply_css(self):
+        css_provider = Gtk.CssProvider()
+        css = """
+            window {
+                background-color: #2e2f31;
+            }
+            button {
+                background-color: #2e2f31;
+                border: none;
+                border-radius: 5px;
+                padding: 10px;
+                box-shadow: none;
+            }
+            button:hover {
+                background-color: #535353;
+            }
+            label {
+                font-family: Dejavu Sans;
+                font-size: 18px;
+                color: #ffffff;
+            }
+            label.header {
+                font-family: Dejavu Sans;
+                font-size: 35px;
+                font-weight: bold;
+                margin-bottom: 15px;
+            }
+            label.title {
+                font-weight: bold;
+            }
+        """
+        css_provider.load_from_data(css.encode("utf-8"))
+
+        screen = Gdk.Screen.get_default()
+        style_context = Gtk.StyleContext()
+        style_context.add_provider_for_screen(
+            screen, css_provider, Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION
+        )
+
+    def create_button(self, icon_name, title, subtitle, command):
+        button = Gtk.Button()
+        box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=10)
+        button.add(box)
+
+        icon = Gtk.Image.new_from_icon_name(icon_name, Gtk.IconSize.DIALOG)
+        icon.set_pixel_size(48)
+
+        box.pack_start(icon, False, False, 0)
+        label_box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=2)
+
+        title_label = Gtk.Label(label=title)
+        title_label.get_style_context().add_class("title")
+        title_label.set_xalign(0)
+
+        subtitle_label = Gtk.Label(label=subtitle)
+        subtitle_label.set_xalign(0)
+
+        label_box.pack_start(title_label, False, False, 0)
+        label_box.pack_start(subtitle_label, False, False, 0)
+
+        box.pack_start(label_box, True, True, 0)
+        button.connect("clicked", lambda widget: subprocess.run(command))
+        return button
+
+
+if __name__ == "__main__":
+    win = WelcomeWindow()
+    win.connect("destroy", Gtk.main_quit)
+    win.show_all()
+    Gtk.main()
